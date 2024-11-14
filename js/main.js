ba8975ae22a3d8ad9379e8f6b2be93a32474a109
vorogurcov <vania.frol2010@yandex.ru>
2024-11-14 11:12:51 +0300

    Implement validation functionality for sign up page. Debug some of the errors made in the previous commits

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from ".
 
 // Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
 // Получаем элемент и меняем его класс, который определеён в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
-
-// В браузере можно посмотреть, что из себя представляет документ
-// (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
-console.log("Document")
-console.log(document)
+// const password = document.getElementById('password');
+// password.classList.add("valid")
+// password.classList.remove("valid")
+//
+// // В браузере можно посмотреть, что из себя представляет документ
+// // (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
+// console.log("Document")
+// console.log(document)
 
 // Если запросить id, которого нет в DOM дереве - вернется undefined
 // => надо быть осторожней: коллега может поменять id вашего элемента и упадёт !ВАШ! код
@@ -39,9 +39,9 @@ const password_repeat_id = 'password-repeat'
 
 const sign_in_link_id = 'sign_in_link'
 const sign_up_form_id = 'sign_up_form'
-// const sign_in_form_id = 'sign_in_form'  // Пригодится
+const sign_in_form_id = 'sign_in_form'  // Пригодится
 const sign_up_btn_id = 'sign_up_btn'
-const sign_in_form_id = 'sign_in_form'
+
 
 
 // Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
@@ -49,13 +49,22 @@ const sign_in_form_id = 'sign_in_form'
 // Значение, которое мы присваеваем этому аттрибуту - это функция, определённая в стрелочном стиле
 // Гуглить по тегам "события JS", "onchange/oninput HTML", "стрелочные функции JS", ...
 
+
+
 const first_name = document.getElementById(first_name_id);
 first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
+
+const last_name = document.getElementById(last_name_id);
+last_name.oninput = (e) => setFormValue(last_name_id, e.target.value)  // Установить значение без валидации
 
 const email = document.getElementById(email_id);
 email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
 
+const password = document.getElementById(password_id);
+password.oninput = (e) => setFormValue(password_id,e.target.value, validatePassword)
 
+const repeated_password = document.getElementById(password_repeat_id);
+repeated_password.oninput = (e) => setFormValue(password_repeat_id,e.target.value, validatePassword)
 
 // Меняем стили объекта DOM дерева. Это позволяет скрыть форму регистрации и показать форму авторизации
 // Объект формы не исключается из DOM дерева, а просто становистя невидимым
--- js/utils.js
+++ js/utils.js
@@ -3,36 +3,39 @@ const last_name_id = 'last_name'
 const password_id = 'password'
 const email_id = 'email'
 const password_repeat_id = 'password-repeat'
+const but_sign_in_id = "sign_in_btn"
+const but_sign_up_id = "sign_up_btn"
 
 // Сюда пишутся значения формы (Object как в Java, или dict из Python)
-const formValues = {first_name_id: '',last_name_id:'',
-                    password_id:'',email_id:'',password_repeat_id:'' }
+const formValues = {[first_name_id]: '',[last_name_id]:'',
+                    [password_id]:'',[email_id]:'',[password_repeat_id]:'' }
 
 // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
 // то при обращении к Object вернётся undefined, который при логическом сравнении обрабатывается как false
-const formValidation = {first_name_id: undefined,last_name_id:undefined,
-                        password_id:undefined,email_id:undefined,password_repeat_id:undefined }
+const formValidation = {[password_id]:undefined,[email_id]:undefined,[password_repeat_id]:undefined }
 
 // Объявляется и инициализируется константная переменная
 // Инициализация функцией, заданной в стрелочном виде
-export const validatePassword = (e) => {
-  formValidation.password = e.target.value
-  console.log("Password validation...")
-  console.log(e)
+export const validatePassword = (pass_id, password) => {
+  const regExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
   // Напишите код валидации здесь и присвойте true/false в объект(словарь) formValidation
   // formValidation.password = ...  // formValidation['password'] = ... - то же самое, но другой синтаксис
-  return formValidation.password !== undefined   // Это заглушка, return вероятно надо переписать
+
+  let result = regExp.test(password)
+  formValidation[pass_id] = result
+  return result
 }
 
 
-export const validateEmail = (email) => {
+export const validateEmail = (em_id,email) => {
   // Создадим шаблон регулярного выражения. В нём применяются шаблонные строки
   // Гуглить по тегам: "шаблонные строки js", "регулярные выражения"
   const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  return String(email)
-    .toLowerCase()
-    .match(regExp);
+  let result = regExp.test(email.toLowerCase())
+  formValidation[em_id] = result
+  return result
+
 }
 
 
@@ -49,7 +52,25 @@ export const getValidationStatus = () => {
 export const setFormValue = (valueKey, newValue, validator) => {
   formValues[valueKey] = newValue
   if (validator !== undefined) {
-    formValidation[valueKey] = validator(newValue)
+    formValidation[valueKey] = validator(valueKey,newValue)
+    console.log(valueKey, " ", formValidation[valueKey])
+    if(formValidation[valueKey] == true) {
+      document.getElementById(valueKey).classList.remove("invalid");
+      document.getElementById(valueKey).classList.add("valid");
+    }
+    else
+    {
+      document.getElementById(valueKey).classList.remove("valid");
+      document.getElementById(valueKey).classList.add("invalid");
+    }
+
+    if (!getValidationStatus())
+    {
+      document.getElementById(but_sign_up_id).disabled = true
+    }
+    else
+      document.getElementById(but_sign_up_id).disabled = false
+
   }
 }
 
